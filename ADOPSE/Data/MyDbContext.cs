1d363e69515d153334dde7afe8527b13e51b79fa
Billy <celanide@gmail.com>
2023-03-30 20:23:54 +0300

    Add MySql settings and define project structure

---
+++ ADOPSE/Data/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ADOPSE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADOPSE.Data;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Categorie> Categorie { get; set; }
+}
