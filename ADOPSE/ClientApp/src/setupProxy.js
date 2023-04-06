4b20802cbe2962e9d060bc7e6f86f2d17d85d972
Thanasis Karezos <thankarezos@gmail.com>
2023-04-06 18:53:34 +0300

    fixed port

--- ADOPSE/ClientApp/src/setupProxy.js
+++ ADOPSE/ClientApp/src/setupProxy.js
@@ -1,8 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:27774';
+const target = 'https://localhost:7014'; 
+// const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:27774';
 
 const context = [
   "/weatherforecast",
