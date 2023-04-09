c6ceb5f9b0ea8088701e9251ef17dbf0d2a73a19
Thanasis Karezos <thankarezos@gmail.com>
2023-04-09 06:06:35 +0300

    buildTool

---
+++ buildTool/build.js
@@ -0,0 +1,36 @@
+const { execSync } = require('child_process');
+const { join } = require('path');
+const { copySync, removeSync } = require('fs-extra');
+
+const buildDir = join(__dirname, 'build');
+const clientDir = join(__dirname, '..', 'ADOPSE', 'ClientApp');
+const serverDir = join(__dirname, '..', 'ADOPSE');
+
+const buildClient = () => {
+  console.log('Cleaning client build folder...');
+  removeSync(join(buildDir, 'build'));
+  console.log('Building client...');
+  execSync('npm run build', { cwd: clientDir, stdio: 'inherit' });
+  console.log('Copying client build to server...');
+  copySync(join(clientDir, 'build'), join(buildDir, 'build'));
+}
+
+const buildServer = () => {
+  console.log('Cleaning server build folder...');
+  removeSync(join(buildDir, 'publish'));
+  console.log('Building server...');
+  execSync('dotnet publish -c Release -r linux-x64', { cwd: serverDir, stdio: 'inherit' });
+  console.log('Copying server build to build folder...');
+  copySync(join(serverDir, 'bin', 'Release', 'net7.0', 'linux-x64', 'publish'), join(buildDir, 'publish'));
+}
+
+const command = process.argv[2];
+
+if (command === 'buildClient') {
+  buildClient();
+} else if (command === 'buildServer') {
+  buildServer();
+} else {
+  console.error(`Unknown command: ${command}`);
+  process.exit(1);
+}
