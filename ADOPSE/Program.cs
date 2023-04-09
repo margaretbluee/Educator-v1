871b9f76f15a08cc66c89bc81c37efbea7a048c5
Thanasis Karezos <thankarezos@gmail.com>
2023-04-09 12:28:19 +0300

    allow set of DB

--- ADOPSE/Program.cs
+++ ADOPSE/Program.cs
@@ -1,19 +1,49 @@
 using ADOPSE.Data;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+
+
+// string connectionString;
+
+string connectionString = builder.Configuration.GetConnectionString(
+                        !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DB_NAME")) ? "TestConnection" : "DefaultConnection")
+                        ?? throw new InvalidOperationException("Connection string not found.");
+
+connectionString = connectionString.Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST") ?? "127.0.0.1")
+                                     .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT") ?? "3306")
+                                     .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME") ?? "MyDB")
+                                     .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER") ?? "root")
+                                     .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "root");
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseMySql(connectionString,Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.35-mysql")));
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+Console.Write(connectionString + " Connection String \n"); 
+
+// var options = new DbContextOptionsBuilder<MyDbContext>()
+//     .UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.35-mysql"))
+//     .Options;
+
+
 
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//test DB connection
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    Console.WriteLine(dbContext.Database.CanConnect() ? "Database connected successfully" : "Failed to connect to database");
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
