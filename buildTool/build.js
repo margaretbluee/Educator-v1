cf0e9bb2eb9961257e914b73af2210dc91b30c92
Thanasis Karezos <thankarezos@gmail.com>
2023-04-09 19:06:45 +0300

    Show error stack on connection failure

--- buildTool/build.js
+++ buildTool/build.js
@@ -8,20 +8,20 @@ const serverDir = join(__dirname, '..', 'ADOPSE');
 
 const buildClient = () => {
   console.log('Cleaning client build folder...');
-  removeSync(join(buildDir, 'build'));
+  removeSync(join(buildDir, 'wwwroot'));
   console.log('Building client...');
   execSync('npm run build', { cwd: clientDir, stdio: 'inherit' });
   console.log('Copying client build to server...');
-  copySync(join(clientDir, 'build'), join(buildDir, 'build'));
+  copySync(join(clientDir, 'build'), join(buildDir, 'wwwroot'));
 }
 
 const buildServer = () => {
   console.log('Cleaning server build folder...');
-  removeSync(join(buildDir, 'publish'));
+  removeSync(join(buildDir, 'server'));
   console.log('Building server...');
   execSync('dotnet publish -c Release -r linux-x64', { cwd: serverDir, stdio: 'inherit' });
   console.log('Copying server build to build folder...');
-  copySync(join(serverDir, 'bin', 'Release', 'net7.0', 'linux-x64', 'publish'), join(buildDir, 'publish'));
+  copySync(join(serverDir, 'bin', 'Release', 'net7.0', 'linux-x64', 'publish'), join(buildDir, 'server'));
 }
 
 const command = process.argv[2];
