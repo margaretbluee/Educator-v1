f7b3b06f0f449790080981155106e3b692e8889e
Thanasis Karezos <thankarezos@gmail.com>
2023-04-12 16:53:20 +0300

    testing

--- ADOPSE/ClientApp/src/components/Home.js
+++ ADOPSE/ClientApp/src/components/Home.js
@@ -18,6 +18,7 @@ export function Home () {
         <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
       </ul>
       <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+      <p>this is a test</p>
     </div>
   );
 }
