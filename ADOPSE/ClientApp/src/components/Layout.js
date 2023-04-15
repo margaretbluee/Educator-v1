18dac1141289d58d0aa259bbdf4bc67c9bd9d456
Thanasis Karezos <thankarezos@gmail.com>
2023-04-16 02:19:29 +0300

    removed unused libarries

--- ADOPSE/ClientApp/src/components/Layout.js
+++ ADOPSE/ClientApp/src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useLayoutEffect } from "react";
+import React, { useRef, useState, useLayoutEffect } from "react";
 import { Container } from "reactstrap";
 import NavMenu from "./NavMenu";
 import Footer from "./footer";
--- ADOPSE/ClientApp/src/components/NavMenu/NavMenu.js
+++ ADOPSE/ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, forwardRef } from "react";
+import React, { useState, } from "react";
 import {
   Collapse,
   Navbar,
