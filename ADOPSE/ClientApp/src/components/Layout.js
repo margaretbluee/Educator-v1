7fc4f826311dc95d5facb2aed9eceff7b9153826
Thanasis Karezos <thankarezos@gmail.com>
2023-04-16 02:13:41 +0300

    footer and fixed layout

--- ADOPSE/ClientApp/src/components/Layout.js
+++ ADOPSE/ClientApp/src/components/Layout.js
@@ -1,16 +1,50 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect, useState, useLayoutEffect } from "react";
 import { Container } from "reactstrap";
 import NavMenu from "./NavMenu";
+import Footer from "./footer";
 
-export class Layout extends Component {
-  static displayName = Layout.name;
+const Layout = (props) => {
+  const headerRef = useRef(null);
+  const footerRef = useRef(null);
 
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container tag="main">{this.props.children}</Container>
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [footerHeight, setFooterHeight] = useState(0);
+
+  const handleSize = () => {
+    console.log(headerRef?.current?.clientHeight ?? 0);
+    console.log(footerRef?.current?.clientHeight ?? 0);
+    setHeaderHeight(headerRef?.current?.clientHeight ?? 0);
+    setFooterHeight(footerRef?.current?.clientHeight ?? 0);
+  };
+
+  useLayoutEffect(() => {
+    handleSize();
+    window.addEventListener("resize", handleSize);
+  }, [headerRef, footerRef]);
+
+  const divStyle = {
+    position: "relative",
+    top: `${headerHeight ?? 0}px`,
+  };
+
+  const containerStyle = {
+    minHeight: `calc(100vh - ${headerHeight ?? 0}px - ${footerHeight ?? 0}px)`,
+    paddingTop: "20px",
+  };
+
+  return (
+    <div>
+      <NavMenu navbarRef={headerRef} />
+      <div style={divStyle}>
+        <Container style={containerStyle} tag="main">
+          {props.children}
+        </Container>
+        <Footer footerRef={footerRef} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Layout.displayName = "Layout";
+
+export default Layout;
--- ADOPSE/ClientApp/src/components/NavMenu/NavMenu.js
+++ ADOPSE/ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useRef, forwardRef } from "react";
 import {
   Collapse,
   Navbar,
@@ -10,71 +10,59 @@ import {
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
 
-export default class NavMenu extends Component {
-  static displayName = NavMenu.name;
+function NavMenu({ navbarRef }) {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
 
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar
-          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
-          light
+  return (
+    <header ref={navbarRef} className="fixed-top">
+      <Navbar
+        className="navbar-expand-sm bg-white navbar-toggleable-sm ng-white border-bottom box-shadow"
+        light
+      >
+        <NavbarBrand className="ms-3" tag={Link} to="/">
+          Educator
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="me-2" />
+        <Collapse
+          className="d-sm-inline-flex flex-sm-row-reverse me-2"
+          isOpen={!collapsed}
+          navbar
         >
-          <NavbarBrand className="ms-3" tag={Link} to="/">
-            Educator
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="me-2" />
-          <Collapse
-            className="d-sm-inline-flex flex-sm-row-reverse me-2"
-            isOpen={!this.state.collapsed}
-            navbar
-          >
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">
-                  Dashboard
+          <ul className="navbar-nav flex-grow">
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/">
+                Dashboard
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/counter">
+                Modules
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/fetch-data">
+                My Learning
+              </NavLink>
+            </NavItem>
+            <NavItem className="login-register">
+              <div>
+                <NavLink tag={Link} to="/login" className="text-dark">
+                  Login
                 </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">
-                  Modules
+                <span className="text-dark"> / </span>
+                <NavLink tag={Link} to="/register" className="text-dark">
+                  Register
                 </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">
-                  My Learning
-                </NavLink>
-              </NavItem>
-              <NavItem className="login-register">
-                <div>
-                  <NavLink tag={Link} to="/login" className="text-dark">
-                    Login
-                  </NavLink>
-                  <span className="text-dark"> / </span>
-                  <NavLink tag={Link} to="/register" className="text-dark">
-                    Register
-                  </NavLink>
-                </div>
-              </NavItem>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+              </div>
+            </NavItem>
+          </ul>
+        </Collapse>
+      </Navbar>
+    </header>
+  );
 }
+
+NavMenu.displayName = "NavMenu";
+
+export default NavMenu;
