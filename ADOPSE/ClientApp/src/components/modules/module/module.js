72e535e7edc974ea89139391cfccdb8d1b034a0b
Thanasis Karezos <thankarezos@gmail.com>
2023-04-19 17:34:17 +0300

    modules

---
+++ ADOPSE/ClientApp/src/components/modules/module/module.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./module.scss";
+import graph from "./icons/graph.png";
+import users from "./icons/users.png";
+import ratingStar from "./icons/rating-star.png";
+
+function Module(props) {
+  return (
+    <div className="module" key={props.key}>
+      <div className="box">
+        <p className="event-school">{props.school}</p>
+        <p className="event-subject">{props.subject}</p>
+        <p className="event-subtype">{props.subject_type}</p>
+        <img src={graph} alt="" className="vectorIcon"></img>
+        <p className="event-difficulty">{props.difficulty}</p>
+        <img src={ratingStar} alt="" className="ratingIcon" />
+        <p className="event-rating">{props.rating}</p>
+        <img src={users} alt="" className="enrolledIcon" />
+        <p className="event-enrolled">{props.enrolled}</p>
+        {/* <button className="enrolledButton" onClick={enrollStudents}> */}
+        <button className="enrolledButton">Enroll</button>
+      </div>
+    </div>
+  );
+}
+
+export default Module;
