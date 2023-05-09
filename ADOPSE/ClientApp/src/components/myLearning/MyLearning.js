454aa1ec93e72a91eaae33017ff0d78a0fb3ba3f
Thanasis Karezos <thankarezos@gmail.com>
2023-05-09 16:02:31 +0300

    fix my learning

--- ADOPSE/ClientApp/src/components/myLearning/MyLearning.js
+++ ADOPSE/ClientApp/src/components/myLearning/MyLearning.js
@@ -2,51 +2,45 @@ import React, { useEffect, useState } from "react";
 import { hasJWT } from "../authentication/authentication";
 function MyLearning() {
   const [modules, setModules] = useState([]);
-  const fetchModules = async () => {
-    try {
-      const headers = {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      };
-      const response = await fetch("/api/enrolled", { headers });
 
-            if (response.ok) {
-                const data = await response.json();
-                setModules(data)
-                console.log("Login response " + modules)
-                console.log(data)
-            } else {
-                throw new Error('Error fetching');
-            }
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const headers = {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        };
+        const response = await fetch("/api/enrolled", { headers });
+
+        if (response.ok) {
+          const data = await response.json();
+          setModules(data);
+          // console.log("Login response " + modules)
+          console.log(data);
+        } else {
+          throw new Error("Error fetching");
         }
-        catch (error) {
-            console.error(error);
-        }
-    }
-    
-    useEffect(
-        () => {
-            fetchModules();
-        },[])
-    
-    return (
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchModules();
+  }, []);
+
+  return (
+    <div>
+      {hasJWT() ? (
         <div>
-            {
-                hasJWT() ? 
-                    <div>
-                        <ul>
-                            {
-                                modules.map((module) => (
-                                    <li>{module.name}</li>
-                                ))
-                            }
-                        </ul>
-                    </div> :
-                    <div>
-                        No auth
-                    </div>
-            }
+          <ul>
+            {modules.map((module) => (
+              <li key={module.id}>{module.name}</li>
+            ))}
+          </ul>
         </div>
-    );
+      ) : (
+        <div>No auth</div>
+      )}
+    </div>
+  );
 }
 
 export default MyLearning;
