0905fe8b186d8d0dee2c078cb547995d02755289
CelaniDe <celanide@gmail.com>
2023-05-09 15:31:43 +0300

    Update MyLearning.js

--- ADOPSE/ClientApp/src/components/myLearning/MyLearning.js
+++ ADOPSE/ClientApp/src/components/myLearning/MyLearning.js
@@ -1,6 +1,32 @@
-﻿import React from 'react';
+﻿import React, {useEffect, useState} from 'react';
 import {hasJWT} from "../authentication/authentication";
 function MyLearning() {
+    
+    const [modules, setModules] = useState([])
+    const fetchModules = async () => {
+        try {
+            const headers = { 'Authorization': `Bearer ${localStorage.getItem('token')}` };
+            const response = await fetch('/api/enrolled', {headers});
+
+            if (response.ok) {
+                const data = await response.json();
+                setModules(data)
+                console.log("Login response " + modules)
+                console.log(data)
+            } else {
+                throw new Error('Error fetching');
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    
+    useEffect(
+        () => {
+            fetchModules();
+        },[])
+    
     return (
         <div>
             {
