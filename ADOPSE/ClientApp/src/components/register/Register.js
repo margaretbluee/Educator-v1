0e6d83545cbe32e5cba850c28575d27cd800f083
Thanasis Karezos <thankarezos@gmail.com>
2023-05-14 16:31:09 +0300

    fixed unused

--- ADOPSE/ClientApp/src/components/register/Register.js
+++ ADOPSE/ClientApp/src/components/register/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Register.scss";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { message } from "antd";
 
 function Register(props) {
