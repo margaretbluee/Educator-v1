a92f75c1d0ecc0f0fa55969316c7e600fb0fe6aa
Thanasis Karezos <thankarezos@gmail.com>
2023-05-21 07:23:14 +0300

    fix test

--- ADOPSE/ClientApp/src/App.test.js
+++ ADOPSE/ClientApp/src/App.test.js
@@ -5,25 +5,11 @@ import App from "./App";
 import { JSDOM } from "jsdom";
 import fetch from "node-fetch";
 
-// Create a JSDOM environment
-const { window } = new JSDOM("<!doctype html><html><body></body></html>", {
-  url: "http://localhost",
-});
-
-// Configure the cookie settings for the JSDOM environment
 window.document.cookieJar = {
   allowSpecialUseDomain: true,
   rejectPublicSuffixes: false,
 };
 
-// Assign the JSDOM window to the global object
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: "node.js",
-};
-
-// Provide a global fetch function using node-fetch
 global.fetch = fetch;
 
 it("renders without crashing", async () => {
